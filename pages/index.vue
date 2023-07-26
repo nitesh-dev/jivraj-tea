ae4b2a2bda39d122fda55ee6c3670e1a404ad256
nitesh-dev <niteshdev547@gmail.com>
2023-07-26 11:25:54 +0530

    basic setup

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<script setup lang='ts'>
+</script>
+<template>
+    <h1>Hello starter</h1>
+    <h1>Hello starter</h1>
+</template>
+<style scoped>
+h1 {
+    color: rgb(148, 24, 24);
+    font-weight: bold;
+}
+</style>
