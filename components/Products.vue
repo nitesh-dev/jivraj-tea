380fb93226b609fa2bffd3088f80ac284b966fb2
nitesh-dev <niteshdev547@gmail.com>
2023-07-27 15:09:36 +0530

    js added in product scrollbar

--- components/Products.vue
+++ components/Products.vue
@@ -1,4 +1,17 @@
 <script setup lang='ts'>
+
+const scrollContainer = ref<HTMLDivElement>()
+
+function scrollLeft(){
+    if(scrollContainer.value == undefined) return
+    scrollContainer.value.scrollLeft = 0
+}
+
+function scrollRight(){
+    if(scrollContainer.value == undefined) return
+    scrollContainer.value.scrollLeft = scrollContainer.value.scrollWidth
+}
+
 </script>
 <template>
 
@@ -8,13 +21,13 @@
             <div class="heading">
                 <h2>Discover Our Products Line</h2>
                 <div class="slider">
-                    <button class="icon-button">
+                    <button @click="scrollLeft" class="icon-button">
                         <svg fill="none" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                             <path
                                 d="m4.296 12 8.492-8.727a.75.75 0 1 0-1.075-1.046l-9 9.25a.75.75 0 0 0 0 1.046l9 9.25a.75.75 0 1 0 1.075-1.046L4.295 12Z" />
                         </svg>
                     </button>
-                    <button class="icon-button">
+                    <button @click="scrollRight" class="icon-button">
                         <svg width="24" height="24" fill="none" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                             <path
                                 d="m19.704 12-8.492-8.727a.75.75 0 1 1 1.075-1.046l9 9.25a.75.75 0 0 1 0 1.046l-9 9.25a.75.75 0 1 1-1.075-1.046L19.705 12Z" />
@@ -23,7 +36,7 @@
                 </div>
             </div>
 
-            <div class="products-container hide-scroll">
+            <div ref="scrollContainer" class="products-container hide-scroll">
                 <Product v-for="item in 10"></Product>
             </div>
             <button class="primary">
@@ -59,6 +72,7 @@
     margin: 2rem 0;
     display: flex;
     gap: 12px;
+    scroll-behavior: smooth;
 }
 
 
